350e827e90b771603b54259a644d9f1c120a4b3f
Quasark <q@quasark.xyz>
2023-08-17 15:52:28 -0700

    git is weird

--- sandbox/301.04.sort.js
+++ sandbox/301.04.sort.js
@@ -1,3 +1,28 @@
 'use strict';
+let answer = null;
 
-console.log('hello world');
+function upper(str) {
+  return str.toUpperCase();
+}
+
+function lower(str) {
+  return str.toLowerCase();
+}
+
+const updateAnimal = (arr, callback) => {
+  // Solution code here...
+  return arr.map(callback);
+};
+// const arr = ['BeAr', 'lIon'];
+// answer = updateAnimal(arr, lower);
+
+
+const sortNames = (arr) => {
+  // Solution code here...
+  return arr.sort((arr));
+};
+
+answer = sortNames(['able', 'Bob']);
+
+
+console.log(answer);
